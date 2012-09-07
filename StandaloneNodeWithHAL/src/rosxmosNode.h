837049a46a3ca97eda008dad02246e3e90ea56ef
cyborg-x1 <cyborg-x1@c92f6db2-f8bf-11e1-8b1e-3b27316a5d46>
2012-09-07 08:03:27 +0000

    ros node setup struct comment
    git-svn-id: https://rad-lab.net/repos/sncn@3 c92f6db2-f8bf-11e1-8b1e-3b27316a5d46

--- StandaloneNodeWithHAL/src/rosxmosNode.h
+++ StandaloneNodeWithHAL/src/rosxmosNode.h
@@ -13,13 +13,13 @@
 #include <stdlib.h>
 
 /**
- *	This
+ *	This struct stores the basic ROS node configuration
  */
 typedef struct
 {
-	char *node_name; 				//!< The name of the node, must be <b>unique</b> in whole ROS!
-	char *ros_master_host; 			//!< Hostname of the computer running roscore
-	unsigned int ros_master_port; //!< RPC port of roscore, standard is: 11311
+	char 			*node_name; 			//!< The name of the node, must be <b>unique</b> in whole ROS!
+	char 			*ros_master_host; 		//!< Hostname of the computer running roscore
+	unsigned int 	ros_master_port; 		//!< RPC port of roscore, standard is: 11311
 }ros_node_setup;
 
 /**
