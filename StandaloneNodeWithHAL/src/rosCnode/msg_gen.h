04301de90735f3a9f0db998723cb0ff5a8ebc81d
cyborg-x1 <cyborg-x1@c92f6db2-f8bf-11e1-8b1e-3b27316a5d46>
2012-09-13 06:46:12 +0000

    Corrected Headers
    git-svn-id: https://rad-lab.net/repos/sncn@27 c92f6db2-f8bf-11e1-8b1e-3b27316a5d46

--- StandaloneNodeWithHAL/src/rosCnode/msg_gen.h
+++ StandaloneNodeWithHAL/src/rosCnode/msg_gen.h
@@ -3,22 +3,22 @@
  *
  *  msg_gen.h
  *
- *  This file is part of StandaloneNodeWithHAL.
+ *  This file is part of ROScNode Library.
  *
- *  StandaloneNodeWithHAL is free software: you can redistribute it and/or modify
+ *  ROScNode Library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  StandaloneNodeWithHAL is distributed in the hope that it will be useful,
+ *  ROScNode Library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
  *
  *  You should have received a copy of the GNU Affero General Public License
- *  along with StandaloneNodeWithHAL. If not, see <http://www.gnu.org/licenses/>.
+ *  along with ROScNode Library. If not, see <http://www.gnu.org/licenses/>.
  *
- *	File created by choll on 11.09.2012
+ *	File created by Christian Holl on 11.09.2012
  */
 
 
