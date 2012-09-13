a95bf73a6b7a3f34534d811fa248cf73a75a2f8a
cyborg-x1 <cyborg-x1@c92f6db2-f8bf-11e1-8b1e-3b27316a5d46>
2012-09-13 09:12:19 +0000

    git-svn-id: https://rad-lab.net/repos/sncn@29 c92f6db2-f8bf-11e1-8b1e-3b27316a5d46

--- StandaloneNodeWithHAL/src/rosCnode/msg_gen.c
+++ StandaloneNodeWithHAL/src/rosCnode/msg_gen.c
@@ -6,16 +6,16 @@
  *  This file is part of ROScNode Library.
  *
  *  ROScNode Library is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
+ *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  ROScNode Library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
+ *  You should have received a copy of the GNU General Public License
  *  along with ROScNode Library. If not, see <http://www.gnu.org/licenses/>.
  *
  *	File created by Christian Holl on 11.09.2012
@@ -82,39 +82,63 @@ void int2buf(char* message_buffer, unsigned int *buf_index, unsigned int number)
 	}
 }
 
-
 unsigned int generateHTTPHeader(char* message_buffer, const http_head_gen_command* gen_array, const char **custom_string_array)
 {
 	unsigned int buf_index=0;
-	while(*gen_array != HTTP_HEADER_GEN_END)
+	const char *outstring;
+	int outmode;
+	unsigned int number;
+	int command=*gen_array;
+
+	while(command != HTTP_HEADER_GEN_END)
 	{
-		if((*gen_array) == HTTP_HEADER_GEN_CUSTOM_TEXT_END)
+		if(command == HTTP_HEADER_GEN_CUSTOM_TEXT_END)
 		{
-			message_buffer[buf_index]='\n';
-			buf_index++;
+			outmode=S2B_NORMAL;
+			outstring="\n";
 		}
-		else if((*gen_array)>=HTTP_HEADER_GEN_VAL_UINT_NUMBER)
+		else if(command>=HTTP_HEADER_GEN_VAL_UINT_NUMBER)
 		{
-			unsigned int number=(*gen_array)-HTTP_HEADER_GEN_VAL_UINT_NUMBER;
+			number=command-HTTP_HEADER_GEN_VAL_UINT_NUMBER;
+			//int2buf(message_buffer,&buf_index,number);
+			outmode=-1;
+		}
+		else if(command>=HTTP_HEADER_GEN_VAL_CUSTOM) //Print custom value, no newline...
+		{
+			//str2buf(&buf_index,message_buffer,custom_string_array[command-HTTP_HEADER_GEN_VAL_CUSTOM],S2B_NORMAL);
+			outstring=custom_string_array[command-HTTP_HEADER_GEN_VAL_CUSTOM];
+			outmode=S2B_NORMAL;
+		}
+		else if(command>=HTTP_HEADER_VALUE_BEGIN) //Print std value
+		{
+			//str2buf(&buf_index,message_buffer,http_header_stdtext[command-HTTP_HEADER_VALUE_BEGIN],S2B_HTTP_HEAD_FIELD);
+			outstring=http_header_stdtext[command-HTTP_HEADER_VALUE_BEGIN];
+			outmode=S2B_HTTP_HEAD_FIELD;
+		}
+		else if(command>=HTTP_HEADER_GEN_DESC_CUSTOM) //Print custom descriptor
+		{
+			//str2buf(&buf_index,message_buffer,custom_string_array[command-HTTP_HEADER_GEN_DESC_CUSTOM],S2B_HTTP_HEAD_FIELD_DESC);
+			outstring=custom_string_array[command-HTTP_HEADER_GEN_DESC_CUSTOM];
+			outmode=S2B_HTTP_HEAD_FIELD_DESC;
+		}
+		else if(command>=HTTP_HEADER_DESC_BEGIN) //Print std descriptor
+		{
+			//str2buf(&buf_index,message_buffer,http_header_descriptors[command-HTTP_HEADER_DESC_BEGIN],S2B_HTTP_HEAD_FIELD_DESC);
+			outstring=http_header_descriptors[command-HTTP_HEADER_DESC_BEGIN];
+			outmode=S2B_HTTP_HEAD_FIELD_DESC;
+		}
+
+		if(outmode<0)
+		{
 			int2buf(message_buffer,&buf_index,number);
 		}
-		else if((*gen_array)>=HTTP_HEADER_GEN_VAL_CUSTOM) //Print custom value, no newline...
+		else
 		{
-			str2buf(&buf_index,message_buffer,custom_string_array[(*gen_array)-HTTP_HEADER_GEN_VAL_CUSTOM],S2B_NORMAL);
+			str2buf(&buf_index, message_buffer,outstring,outmode);
 		}
-		else if((*gen_array)>=HTTP_HEADER_VALUE_BEGIN) //Print std value
-		{
-			str2buf(&buf_index,message_buffer,http_header_stdtext[(*gen_array)-HTTP_HEADER_VALUE_BEGIN],S2B_HTTP_HEAD_FIELD);
-		}
-		else if((*gen_array)>=HTTP_HEADER_GEN_DESC_CUSTOM) //Print custom descriptor
-		{
-			str2buf(&buf_index,message_buffer,custom_string_array[(*gen_array)-HTTP_HEADER_GEN_DESC_CUSTOM],S2B_HTTP_HEAD_FIELD_DESC);
-		}
-		else if((*gen_array)>=HTTP_HEADER_DESC_BEGIN) //Print std descriptor
-		{
-			str2buf(&buf_index,message_buffer,http_header_descriptors[(*gen_array)-HTTP_HEADER_DESC_BEGIN],S2B_HTTP_HEAD_FIELD_DESC);
-		}
+
 		gen_array++;
+		command=*gen_array;
 	}
 	//Finish Header with empty line
 	message_buffer[buf_index]='\n';
@@ -131,77 +155,76 @@ unsigned int generateXML(char* message_buffer, const ros_rpc_gen_command* gen_ar
 	int outmode;
 	unsigned int number;
 	int command=*gen_array;
-
 	while(command != RPC_GENERATOR_FINISH)
 	{
-			/*
-			 * UNSIGNED INTEGER
-			 */
-			if(command>=RPC_UINT_NUMBER)
+		/*
+		 * UNSIGNED INTEGER
+		 */
+		if(command>=RPC_UINT_NUMBER)
+		{
+			number=command-RPC_UINT_NUMBER;
+			outmode=-1;
+		}
+		/*
+		 * TEXT FIELD
+		 */
+		else if(command>=RPC_STDTEXT_START-1) //Add text
+		{
+			if(command>=RPC_CUSTOM_TEXT)
 			{
-				number=command-RPC_UINT_NUMBER;
-				outmode=-1;
-			}
-			/*
-			 * TEXT FIELD
-			 */
-			else if(command>=RPC_STDTEXT_START-1) //Add text
-			{
-				if(command>=RPC_CUSTOM_TEXT)
-				{
-					outstring=custom_string_array[command-RPC_CUSTOM_TEXT];
-					outmode=S2B_NORMAL;
-				}
-				else
-				{
-					outstring=ros_rpc_stdtext[command-RPC_STDTEXT_START-1];
-					outmode=S2B_NORMAL;
-				}
-			}
-			/*
-			 * CLOSING TAG
-			 */
-			else if(command>=RPC_CLOSE_TAG)
-			{
-				if(command>=RPC_CUSTOM_TAG+RPC_CLOSE_TAG)
-				{
-					outstring=custom_string_array[command-RPC_CUSTOM_TAG-RPC_CLOSE_TAG];
-					outmode=S2B_CTAG;
-				}
-				else
-				{
-					outstring=ros_rpc_tag_strings[command-RPC_TAGS_START-1-RPC_CLOSE_TAG];
-					outmode=S2B_CTAG;
-				}
-			}
-			/*
-			 * OPENING TAG
-			 */
-			else if(command>=RPC_TAGS_START-1)
-			{
-				if(command>=RPC_CUSTOM_TAG)
-				{
-					outstring=custom_string_array[command-RPC_CUSTOM_TAG];
-					outmode=S2B_TAG;
-				}
-				else
-				{
-					outstring=ros_rpc_tag_strings[command-RPC_TAGS_START-1];
-					outmode=S2B_TAG;
-				}
-			}
-
-			if(outmode<0)
-			{
-				int2buf(message_buffer,&buf_index,number);
+				outstring=custom_string_array[command-RPC_CUSTOM_TEXT];
+				outmode=S2B_NORMAL;
 			}
 			else
 			{
-				str2buf(&buf_index, message_buffer,outstring,outmode);
+				outstring=ros_rpc_stdtext[command-RPC_STDTEXT_START-1];
+				outmode=S2B_NORMAL;
 			}
+		}
+		/*
+		 * CLOSING TAG
+		 */
+		else if(command>=RPC_CLOSE_TAG)
+		{
+			if(command>=RPC_CUSTOM_TAG+RPC_CLOSE_TAG)
+			{
+				outstring=custom_string_array[command-RPC_CUSTOM_TAG-RPC_CLOSE_TAG];
+				outmode=S2B_CTAG;
+			}
+			else
+			{
+				outstring=ros_rpc_tag_strings[command-RPC_TAGS_START-1-RPC_CLOSE_TAG];
+				outmode=S2B_CTAG;
+			}
+		}
+		/*
+		 * OPENING TAG
+		 */
+		else if(command>=RPC_TAGS_START-1)
+		{
+			if(command>=RPC_CUSTOM_TAG)
+			{
+				outstring=custom_string_array[command-RPC_CUSTOM_TAG];
+				outmode=S2B_TAG;
+			}
+			else
+			{
+				outstring=ros_rpc_tag_strings[command-RPC_TAGS_START-1];
+				outmode=S2B_TAG;
+			}
+		}
 
-			gen_array++;
-			command=*gen_array;
+		if(outmode<0)
+		{
+			int2buf(message_buffer,&buf_index,number);
+		}
+		else
+		{
+			str2buf(&buf_index, message_buffer,outstring,outmode);
+		}
+
+		gen_array++;
+		command=*gen_array;
 	}
 	return buf_index;
 }
--- StandaloneNodeWithHAL/src/rosCnode/msg_strings.h
+++ StandaloneNodeWithHAL/src/rosCnode/msg_strings.h
@@ -6,16 +6,16 @@
  *  This file is part of ROScNode Library.
  *
  *  ROScNode Library is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
+ *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  ROScNode Library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
+ *  You should have received a copy of the GNU General Public License
  *  along with ROScNode Library. If not, see <http://www.gnu.org/licenses/>.
  *
  *	File created by Christian Holl on 11.09.2012
