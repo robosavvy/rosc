61b7c7fa7ff1592966d47f332afc20ef8c11cb97
cyborg-x1 <cyborg-x1@c92f6db2-f8bf-11e1-8b1e-3b27316a5d46>
2013-01-29 21:53:39 +0000

    adds message examples to test working principles
    git-svn-id: https://rad-lab.net/repos/sncn@97 c92f6db2-f8bf-11e1-8b1e-3b27316a5d46

---
+++ rosc_example_messages/include/rosc/geometry_msgs/Twist.h
@@ -0,0 +1,10 @@
+/*
+ * NON GENERATED EXAMPLE
+ */
+
+#ifndef TWIST_H_
+#define TWIST_H_
+
+
+
+#endif /* TWIST_H_ */
---
+++ rosc_example_messages/src/rosc/geometry_msgs/Vector3.c
@@ -0,0 +1,40 @@
+/*
+ *  Copyright by Synapticon GmbH (www.synapticon.com)  ?2013
+ *
+ *  Vector3.c
+ *
+ *  This file is part of rosc_example_messages.
+ *
+ *  rosc_example_messages is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  rosc_example_messages is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with rosc_example_messages. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	File created by choll on Jan 29, 2013
+ */
+
+
+//#include <rosc/rosc.h>
+#include <inttypes.h>
+#include <float.h>
+
+/*
+ * Message Definition
+ * # This represents a vector in free space.
+ * float64 x
+ * float64 y
+ * float64 z
+ */
+
+typedef struct geometry_msgs_Vector3_t
+{
+
+};
