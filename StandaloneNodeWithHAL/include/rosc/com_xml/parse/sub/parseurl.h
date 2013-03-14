553dc9872d09bfe259fa4217820c7100b31c680c
cyborg-x1 <cyborg-x1@c92f6db2-f8bf-11e1-8b1e-3b27316a5d46>
2013-03-14 11:56:44 +0000

    removes some files and moves their contents to parser_structure.h
    git-svn-id: https://rad-lab.net/repos/sncn@114 c92f6db2-f8bf-11e1-8b1e-3b27316a5d46

--- StandaloneNodeWithHAL/include/rosc/com_xml/parse/sub/parseurl.h
+++ StandaloneNodeWithHAL/include/rosc/com_xml/parse/sub/parseurl.h
@@ -2,15 +2,16 @@
 #ifndef PARSEURL_H_
 #define PARSEURL_H_
 
+/**
+ *	Contains the result states parse url submode
+ */
 typedef enum
 {
-	PARSE_URL_MATCH_HOSTNAME,
-	PARSE_URL_MATCH_IPv4,
-	PARSE_URL_MATCH_IPv6,
-	PARSE_URL_MATCH_IPv6_RESOLV,
+	PARSE_URL_MATCH_HOSTNAME,   //!< PARSE_URL_MATCH_HOSTNAME - means that the current content inside the buffer will only match a hostname
+	PARSE_URL_MATCH_IPv4,       //!< PARSE_URL_MATCH_IPv4 - means that the content is a IPv4 address
+	PARSE_URL_MATCH_IPv6,       //!< PARSE_URL_MATCH_IPv6 - means that the content is a IPv6 address
+	PARSE_URL_MATCH_IPv6_RESOLV,//!< PARSE_URL_MATCH_IPv6_RESOLV - means that the content is a IPv6 address with a IPv4 network resolving addition
 }parse_url_match_t;
-
-
 
 
 #endif /* PARSEURL_H_ */
